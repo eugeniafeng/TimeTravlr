2e2f7e19a9c46638e497536d1e09ee1749f6e701
eugeniafeng <eugeniafeng@gmail.com>
2020-09-19 16:11:41 -0400

    fixed conflicts

--- TimeTravlr/GameViewController.swift
+++ TimeTravlr/GameViewController.swift
@@ -13,9 +13,8 @@ class GameViewController: UIViewController {
     let places: [UIImage: Int] = [UIImage(named: "1902 Hawaii")!: 1902, UIImage(named: "1914 Porterville California")!: 1914, UIImage(named: "1915 Yosemite")!: 1915, UIImage(named: "1929 New York")!: 1929, UIImage(named: "1947 Philadelphia")!: 1947, UIImage(named: "1950 Old Faithful")!: 1950, UIImage(named: "1966 London")!: 1966, UIImage(named: "1971 New York")!: 1971, UIImage(named: "2016 Mount Rainier")!: 2016, UIImage(named: "2018 Yosemite")!: 2018]
     let baseImages: [UIImage] = [UIImage(named: "1902 Hawaii")!, UIImage(named: "1914 Porterville California")!, UIImage(named: "1915 Yosemite")!, UIImage(named: "1929 New York")!, UIImage(named: "1947 Philadelphia")!, UIImage(named: "1950 Old Faithful")!, UIImage(named: "1966 London")!, UIImage(named: "1971 New York")!, UIImage(named: "2016 Mount Rainier")!, UIImage(named: "2018 Yosemite")!]
     
-    let URL_IMAGE = URL(string: "https://earthengine.googleapis.com/v1alpha/projects/earthengine-legacy/thumbnails/0d5fb7a94b62bfc7848ec19ce2cc90b7-27172365d41070e0324d994e0bbb278c:getPixels")
-
     let URL_IMAGE = URL(string: "https://image.shutterstock.com/image-vector/sample-stamp-grunge-texture-vector-260nw-1389188336.jpg")
+    
     let randomInt = Int.random(in: 0..<10)
     
     @IBOutlet weak var gameImage: UIImageView!
@@ -27,7 +26,8 @@ class GameViewController: UIViewController {
         // Do any additional setup after loading the view.
         // gameImage.image =
         createTapGestureForRemovingKeyboard()
-//        let session = URLSession(configuration: .default)
+        gameImage.image = baseImages[randomInt]
+
         let session = URLSession(configuration: .default)
         
         let getImageFromUrl = session.dataTask(with: URL_IMAGE!) { (data, response, error) in
@@ -52,31 +52,7 @@ class GameViewController: UIViewController {
             }
         }
         getImageFromUrl.resume()
-//        let session = URLSession(configuration: .default)
-//
-//        let getImageFromUrl = session.dataTask(with: URL_IMAGE!) { (data, response, error) in
-//            if let e = error{
-//                print("Some error occurred: \(e)")
-//            } else {
-//
-//                if(response as? HTTPURLResponse) != nil{
-//
-//                    if let imageData = data{
-//                        let image = UIImage(data: imageData)
-//
-//                        self.gameImage.image = image
-//                    } else {
-//                        print("no image  found")
-//                    }
-//
-//                } else{
-//                    print("No response from server")
-//                }
-//            }
-//        }
-//        getImageFromUrl.resume()
-        
-        gameImage.image = baseImages[randomInt]
+    }
         
     
     
