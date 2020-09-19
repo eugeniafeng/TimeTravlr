15c3c8832a81a0cb4f02a89d713431fb058b79ee
eugeniafeng <eugeniafeng@gmail.com>
2020-09-19 13:38:21 -0400

    Added color scheme to storyboard

--- TimeTravlr/GameViewController.swift
+++ TimeTravlr/GameViewController.swift
@@ -16,6 +16,7 @@ class GameViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        // gameImage.image =
     }
     
     
